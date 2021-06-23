eec37d7c29e096ed71e3d16a8adcc64339c2cac3
Hillary Mongare <hillarymongare70@gmail.com>
2021-06-23 16:02:36 +0300

    scrapping using beautiful soup

--- Day 2/twitter_scraping.py
+++ Day 2/twitter_scraping.py
@@ -1,23 +1,15 @@
-import tweepy
+from bs4 import BeautifulSoup
+import requests
 
-consumer_key = 'XOANlh46iHzOLpYrwOL45xP9w'
-consumer_secret = '1tqbkg19Qa1xc127eyQ5oUJ1Teodo2CkvX2JYHvGZn1zUieZ7P'
-access_token = '214906456-lhVmvjuIqtclbOtoT0fBeU1iB6IztyoSZP4LOJf7'
-access_token_secret = "r8mnZtu6foJSyFixkPFIoVUfaQAdeoAHKZDQyp8oDr8ig"
+handle = input("Input your account name on Twitter: ")
 
+temp = requests.get('https://twitter.com/'+handle)
+bs = BeautifulSoup(temp.text, 'lxml')
 
-#function to authenticate to Twitter
-def connect():
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+try:
+    follow_box = bs.find('li',{'class':'ProfileNav-item ProfileNav-item--followers'})
+    followers = follow_box.find('a').find('span',{'class':'ProfileNav-value'})
+    print("Number of followers: {}".format(followers.get('data-count')))
 
-    api = tweepy.API(auth)
-
-    try: 
-        api.verify_credentials()
-        print("Authentication OK")
-        
-    except:
-        print("Error during authentication")
-
-connect()
+except:
+    print('Account name not found...')
