fbb843e36199714e10a3df08835accb07c7d9e4f
Kihara Njoroge <46190291+Babuuh@users.noreply.github.com>
2021-06-24 19:39:13 +0300

    question 1 changes

--- Day 3/Q1.py
+++ Day 3/Q1.py
@@ -1,7 +1,15 @@
 import pandas as pd
+import pdfkit
 
-df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-17-2020.csv')
+#fetching the dataset
+data = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-17-2020.csv', usecols=['Last Update', 'Country/Region', 'Confirmed','Deaths', 'Recovered'])
+top_20 = data.groupby('Country/Region').max().sort_values(by='Confirmed', ascending=False)[:20]
 
-df = df[['Last Update', 'Country/Region', 'Confirmed', 'Deaths', 'Recovered']].head(20)
+#converting the above data into html
+file = open('data.html', 'w')
+top_20_html = top_20.to_html()
+file.write(top_20_html)
+file.close()
 
-print(df)
+#converting the html to pdf
+pdfkit.from_file('data.html','covid19_top_20_countries.pdf')
