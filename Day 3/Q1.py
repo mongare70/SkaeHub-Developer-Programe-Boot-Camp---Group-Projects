738f596aa5989a943e6cbfaa49765eb64530efd2
Hillary Mongare <hillarymongare70@gmail.com>
2021-06-24 17:04:24 +0300

    implementing pandas module 1

---
+++ Day 3/Q1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-17-2020.csv')
+
+df = df[['Last Update', 'Country/Region', 'Confirmed', 'Deaths', 'Recovered']].head(20)
+
+print(df)
+
